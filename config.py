245e4f922ecc65d5bf1eea5bcb60ea83ee4f140c
tyZie <tyzieluter@mail.ru>
2025-05-18 22:03:56 +0300

    added log files, changed log format

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 #Versions
-console_version = '1.3.0-alpha'
+console_version = '1.3.1-alpha'
 modules_version = '1.0.1-alpha'
 
 #User settings
 username = 'user'
 use_logs = True
+log_max_size = "100 MB" # Use 'x KB' or 'x MB'
--- logs_contoller.py
+++ logs_contoller.py
@@ -1,6 +1,28 @@
+from pathlib import Path
+import sys
+
 from loguru import logger
 
 import config
+
+if config.use_logs == True:
+    log_path = Path('logs')
+    log_path.mkdir(exist_ok=True)
+
+    logger.remove()
+
+    logger.add(
+        log_path / "terminode_{time:YY-MM-DD}_{time:HH-mm}.log",
+        rotation=config.log_max_size,
+        retention="7 days",
+        compression="zip",
+        level="DEBUG",
+        format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}"
+)
+    logger.add(
+        sys.stderr,
+        format="{time:HH:mm:ss} | <level>{level}</level> | {message}"
+)
 
 def create_log(text: str, type: str):
     if config.use_logs == True:
